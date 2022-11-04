245c4478d32b917d4606ace6f27d294fe14f6b9c
Dario <dario.25@live.it>
2022-11-04 15:42:53 +0100

    Code refactoring script.js

--- script.js
+++ script.js
@@ -1,156 +1,162 @@
-// Object
-const ps5 = {
-    // Properties
-    status : false,
+// PS5 Object Model
+    const ps5 = {
+        // Properties
+        status : false,
 
-    memory : 825,
+        memory : 825,
 
-    games: [],
+        games: [],
 
-    // Functions
-    on : function() {
-        this.status = true;
-    },
+        // Functions
+        on : function() {
+            this.status = true;
+        },
 
-    off : function() {
-        this.status = false;
-    },
+        off : function() {
+            this.status = false;
+        },
 
-    installGame : function(game) {
-        this.games.push(game);
+        installGame : function(game) {
+            this.games.push(game);
+        }
     }
-}
 
-// DOM
-let turnOnBtn = document.querySelector("#turnOnBtn");
-let turnOffBtn = document.querySelector("#turnOffBtn");
+// On-Off Buttons
+    let turnOnBtn = document.querySelector("#turnOnBtn");
+    let turnOffBtn = document.querySelector("#turnOffBtn");
 
 // Installed Games Button
-let installedGames = document.querySelector("#installedGames");
+    let installedGames = document.querySelector("#installedGames");
 
 // Shop Buttons
-let shopBtn = document.querySelector("#shopBtn");
-let shopBackBtn = document.querySelector("#shopBackBtn");
+    let shopBtn = document.querySelector("#shopBtn");
+    let shopBackBtn = document.querySelector("#shopBackBtn");
 
 // Main Wrapper Area
-let startWrapper = document.querySelector(".startWrapper");
-let menuWrapper = document.querySelector(".menuWrapper");
-let shopWrapper = document.querySelector(".shopWrapper");
+    let startWrapper = document.querySelector(".startWrapper");
+    let menuWrapper = document.querySelector(".menuWrapper");
+    let shopWrapper = document.querySelector(".shopWrapper");
 
 // Shop Cards List
-let shopCardsList = document.querySelector(".shopCardsList");
+    let shopCardsList = document.querySelector(".shopCardsList");
 
-// ON/OFF Buttons
-turnOnBtn.addEventListener("click", () => {
-    ps5.on();
-    startWrapper.classList.add("d-none");
-    setTimeout(() => {
+// ON/OFF Logic
+    turnOnBtn.addEventListener("click", () => {
+        ps5.on();
+        startWrapper.classList.add("d-none");
+        setTimeout(() => {
+            menuWrapper.classList.remove("d-none");
+        }, 1);
+        console.log("Success ON");
+    });
+
+    turnOffBtn.addEventListener("click", () => {
+        ps5.off();
+        menuWrapper.classList.add("d-none");
+        setTimeout(() => {
+            startWrapper.classList.remove("d-none");
+        }, 1);
+        console.log("Success OFF");
+    });
+
+// Installed Games Section
+    installedGames.addEventListener("click", () => {
+        if(ps5.games.length != 0) {
+            console.table(ps5.games);
+            console.log(`${ps5.games.length} games installed`);
+        } else {
+            console.log("Empty Library");
+        }
+        let totalMemory = 0;
+        ps5.games.forEach(game => {
+            totalMemory += game.space;
+        });
+        console.log(`Used memory: ${totalMemory} GB`);
+        console.log(`Memory left: ${ps5.memory} GB`);
+    });
+
+// Shop Area Logic
+    shopBtn.addEventListener("click", () => {
+        menuWrapper.classList.add("d-none");
+        shopWrapper.classList.remove("d-none");
+        console.log("Enter Shop");
+    });
+
+    shopBackBtn.addEventListener("click", () => {
+        shopWrapper.classList.add("d-none");
         menuWrapper.classList.remove("d-none");
-    }, 1);
-    console.log("Success ON");
-});
+        console.log("Left Shop");
+    });
 
-turnOffBtn.addEventListener("click", () => {
-    ps5.off();
-    menuWrapper.classList.add("d-none");
-    setTimeout(() => {
-        startWrapper.classList.remove("d-none");
-    }, 1);
-    console.log("Success OFF");
-});
+// Game Details Section
+    let gameShopDetailsWrapper = document.querySelector(".gameShopDetailsWrapper");
+    let gameShopDetails = document.querySelector(".gameShopDetails");
+    let gameShopDetailsBackBtn = document.querySelector("#gameShopDetailsBackBtn");
 
-// Shop Area
-shopBtn.addEventListener("click", () => {
-    menuWrapper.classList.add("d-none");
-    shopWrapper.classList.remove("d-none");
-    console.log("Enter Shop");
-});
-
-shopBackBtn.addEventListener("click", () => {
-    shopWrapper.classList.add("d-none");
-    menuWrapper.classList.remove("d-none");
-    console.log("Left Shop");
-});
-
-// Game Shop Details
-let gameShopDetailsWrapper = document.querySelector(".gameShopDetailsWrapper");
-let gameShopDetails = document.querySelector(".gameShopDetails");
-let gameShopDetailsBackBtn = document.querySelector("#gameShopDetailsBackBtn");
-let gameInstallerBtn = document.querySelector("#gameInstallerBtn");
-
-gameShopDetailsBackBtn.addEventListener("click", () => {
-    gameShopDetailsWrapper.classList.add("d-none");
-    shopCardsList.classList.remove("d-none");
-});
+    gameShopDetailsBackBtn.addEventListener("click", () => {
+        gameShopDetailsWrapper.classList.add("d-none");
+        shopCardsList.classList.remove("d-none");
+    });
 
 // Capturing Shop Game Cards After DOM Loading
-setTimeout(() => {
-        let shopCard = document.querySelectorAll(".shopCard");
-        let gameInstallerBtn = [];
-        let installedFlag = [];
-        shopCard.forEach((card, index) => {
-
-            installedFlag[index] = false;
-
-            card.addEventListener("click", () => {
-                gameInstallerBtn[index] = `gameInstallerBtn${index}`;
-                if(installedFlag[index] == false) {
-                    gameShopDetails.innerHTML = 
-                    `
-                        ${card.innerHTML}
-                        <button class="${gameInstallerBtn[index]} menuBtn mb-5">Install Game</button>
-                        <span id="installedTag" class="bg-success text-white p-2 d-none">Installed</span>
-                    `;
-                    installedTag = document.querySelector("#installedTag");
-                    gameInstallerBtn[index] = document.querySelector(`.gameInstallerBtn${index}`);
-                    gameInstallerBtn[index].addEventListener("click", () => {
-                        let game = {
-                            name : card.firstElementChild.innerHTML,
-                            space : parseInt(card.lastElementChild.innerHTML)
-                        };
-                        ps5.installGame(game);
-                        ps5.memory -= game.space;
-                        gameInstallerBtn[index].classList.add("d-none");
-                        installedTag.classList.remove("d-none");
-                        installedFlag[index] = true;
-                    });
-                } else {
-                    gameShopDetails.innerHTML = 
-                    `
-                        ${card.innerHTML}
-                        <span id="installedTag" class="bg-success text-white p-2">Installed</span>
-                    `;
-                }
-
-                shopCardsList.classList.add("d-none");
-                gameShopDetailsWrapper.classList.remove("d-none");
-                console.log("Getting Info");
+    setTimeout(() => {
+            let shopCard = document.querySelectorAll(".shopCard");
+            let gameInstallerBtn = [];
+            let installedFlag = [];
+            shopCard.forEach((card, index) => {
+                installedFlag[index] = false;
+                card.addEventListener("click", () => {
+                    gameInstallerBtn[index] = `gameInstallerBtn${index}`;
+                    if(installedFlag[index] == false) {
+                        gameShopDetails.innerHTML = 
+                        `
+                            ${card.innerHTML}
+                            <button class="${gameInstallerBtn[index]} menuBtn mb-5">Install Game</button>
+                            <span id="installedTag" class="bg-success text-white p-2 d-none">Installed</span>
+                        `;
+                        installedTag = document.querySelector("#installedTag");
+                        gameInstallerBtn[index] = document.querySelector(`.gameInstallerBtn${index}`);
+                        gameInstallerBtn[index].addEventListener("click", () => {
+                            let game = {
+                                name : card.firstElementChild.innerHTML,
+                                space : parseInt(card.lastElementChild.innerHTML)
+                            };
+                            ps5.installGame(game);
+                            ps5.memory -= game.space;
+                            gameInstallerBtn[index].classList.add("d-none");
+                            installedTag.classList.remove("d-none");
+                            installedFlag[index] = true;
+                        });
+                    } else {
+                        gameShopDetails.innerHTML = 
+                        `
+                            ${card.innerHTML}
+                            <span id="installedTag" class="bg-success text-white p-2">Installed</span>
+                        `;
+                    }
+                    shopCardsList.classList.add("d-none");
+                    gameShopDetailsWrapper.classList.remove("d-none");
+                    console.log("Getting Info");
+                });
             });
-        });
-        console.log("### Nodelist Created ###");
-}, 100);
+            console.log("### Nodelist Created ###");
+    }, 100);
 
 // Shop Games List From Json File
-fetch("./shop.json")
-.then(response => response.json())
-.then(data => {
-    // Populating Shop List
-    data.forEach(game => {
-        let gameObj = document.createElement("div");
-        gameObj.classList.add("col-12", "col-lg-6", "col-xl-4", "mb-4");
-        gameObj.innerHTML =
-            `
-                <div class="shopCard">
-                    <h1 class="text-white">${game.name}</h1>
-                    <h3 class="text-white">${game.space} GB</h3>
-                </div>
-            `;
-        shopCardsList.appendChild(gameObj);
-    })
-});
-
-// Installed Games
-installedGames.addEventListener("click", () => {
-    console.table(ps5.games);
-    console.log(`Memory left ${ps5.memory}`);
-});
+    fetch("./shop.json")
+    .then(response => response.json())
+    .then(data => {
+        // Populating Shop List
+        data.forEach(game => {
+            let gameObj = document.createElement("div");
+            gameObj.classList.add("col-12", "col-lg-6", "col-xl-4", "mb-4");
+            gameObj.innerHTML =
+                `
+                    <div class="shopCard">
+                        <h1 class="text-white">${game.name}</h1>
+                        <h3 class="text-white">${game.space} GB</h3>
+                    </div>
+                `;
+            shopCardsList.appendChild(gameObj);
+        })
+    });
